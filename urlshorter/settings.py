589656aa9e6ac29e96aa95e74c47d550c6f64c9b
alval <chaplinskiy@hotmail.com>
2022-07-12 23:47:42 +0300

    added allowed hosts and secret_key from environ

--- urlshorter/settings.py
+++ urlshorter/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)js9do^%flp!=am9bml!7fpjke)ph!979o^k2f^-*2p7#ad-xc'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
