2b6f5fb4d0b7c5986afac587801c46b05b4f6736
alval <chaplinskiy@hotmail.com>
2022-07-13 00:32:06 +0300

    change static root

--- urlshorter/settings.py
+++ urlshorter/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
