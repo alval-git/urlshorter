0181b88ba750a527aeafc43a76668da08de870fa
alval <chaplinskiy@hotmail.com>
2022-07-13 00:02:53 +0300

    secret_key in code

--- urlshorter/settings.py
+++ urlshorter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-)js9do^%flp!=am9bml!7fpjke)ph!979o^k2f^-*2p7#ad-xc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
